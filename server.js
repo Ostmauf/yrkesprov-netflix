3aecc8fbe9c5c045907ca5c91b4f5be42a069fa7
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2022-05-04 09:07:57 +0200

    working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 
-server.use(express.static(__dirname + '/public/index'));
+server.use(express.static(__dirname + '/public'));
 
 server.listen("3000", () => {
     console.log("Server is up!")
